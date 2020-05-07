38581d75ddf6fff1a5a3de2a6d978d398350534d
jgalgarra <jgalgarra@coit.es>
2020-05-07 09:21:49 +0200

    Pruebas JGA

---
+++ kaka.bat
@@ -0,0 +1,4 @@
+python 1_abundancia_edaf_comp.py > log_predictor.txt
+python 1_abundancia_edaf.py >> log_predictor.txt
+python 1_abundancia_competitors.py >> log_predictor.txt
+python 2_competitors.py >> log_predictor.txt
