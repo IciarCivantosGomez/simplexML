0b8ce4ac36262e0e9524cbd44a0ffc1f0f6a3b72
Icíar Civantos Gómez <iciarcivantos@gmail.com>
2020-07-29 14:32:33 +0200

    Borrados ficheros antiguos

--- kaka.bat
+++
@@ -1,4 +0,0 @@
-python 1_abundancia_edaf_comp.py > log_predictor.txt
-python 1_abundancia_edaf.py >> log_predictor.txt
-python 1_abundancia_competitors.py >> log_predictor.txt
-python 2_competitors.py >> log_predictor.txt
